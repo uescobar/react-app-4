7b9b8e7e4b607c0e2ab36f2e1436f2dd595b70e9
uescobar <uescobar.360@gmail.com>
2026-04-15 12:14:03 -0600

    useMutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import { useRef } from "react";
 
 type Post = {
   id: number;
@@ -9,6 +10,24 @@ type Post = {
 };
 
 export default function App() {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const bodyRef = useRef<HTMLInputElement>(null);
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: (post: Post) =>
+      axios
+        .post<Post>("https://jsonplaceholder.typicode.com/posts", post)
+        .then((response) => response.data),
+    onSuccess: (savedPost, newPost) => {
+      queryClient.setQueryData<Post[]>(["post"], (post = []) => [
+        savedPost,
+        ...post,
+      ]);
+
+      // queryClient.invalidateQueries({ queryKey: ["posts"] }); // ir a buscar todo al servidor nuevamente, en este caso no es necesario porque ya tenemos el post guardado en la respuesta de la mutación, pero si queremos ir a buscar todo al servidor nuevamente, por ejemplo para obtener el id generado por el servidor, entonces si sería necesario invalidar la query para que se vuelva a ejecutar y obtener los datos actualizados del servidor
+    },
+  });
+
   const { data, isLoading } = useQuery({
     queryKey: ["posts"],
     queryFn: () =>
@@ -19,9 +38,24 @@ export default function App() {
   return (
     <>
       <h2>Posts</h2>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (titleRef.current?.value && bodyRef.current?.value) {
+            mutate({
+              id: 0,
+              body: bodyRef.current?.value,
+              title: titleRef.current?.value,
+              userId: 1,
+            });
+          }
+        }}
+      >
         <div>
-          <input type="text" />
+          <input ref={titleRef} type="text" placeholder="Título" />
+        </div>
+        <div>
+          <input ref={bodyRef} type="text" placeholder="Cuerpo" />
         </div>
         <div>
           <button>Enviar</button>
