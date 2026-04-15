31531b74d3b8fc0b6670c17f0d1db3d0eced803c
uescobar <uescobar.360@gmail.com>
2026-04-15 10:56:46 -0600

    paginacion

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import useTodos from "./hooks/useTodos";
 
 export default function App() {
-  const [userId, setUserId] = useState<number>();
-
-  const { data, error, isLoading } = useTodos(userId);
+  const pageSize = 15;
+  const [page, setPage] = useState(1);
+  const { data, error, isLoading, isPlaceholderData } = useTodos({
+    page,
+    pageSize,
+  });
 
   if (error) {
     return <h2>{error.message} :(</h2>;
@@ -17,26 +20,18 @@ export default function App() {
   return (
     <>
       <h2>Todos</h2>
-      <select
-        value={userId}
-        onChange={(e) => {
-          if (e.target.value !== "") {
-            setUserId(Number(e.target.value));
-          } else {
-            setUserId(undefined);
-          }
-        }}
-      >
-        <option value="">Todos los usuarios</option>
-        <option value="1">Usuario 1</option>
-        <option value="2">Usuario 2</option>
-        <option value="3">Usuario 3</option>
-      </select>
+
       <ul>
         {data?.map((todo) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
       </ul>
+
+      <button disabled={page == 1} onClick={() => setPage(page - 1)}>
+        {"<<"}
+      </button>
+      <button onClick={() => setPage(page + 1)}>{">>"}</button>
+      {isPlaceholderData && <span>Cargando...</span>}
     </>
   );
 }
