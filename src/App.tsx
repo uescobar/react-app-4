6104a76b5ced5a3c2fdaa849686d62b0a2423285
uescobar <uescobar.360@gmail.com>
2026-04-15 11:13:12 -0600

    Infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import useTodos from "./hooks/useTodos";
 
 export default function App() {
   const pageSize = 15;
-  const [page, setPage] = useState(1);
-  const { data, error, isLoading, isPlaceholderData } = useTodos({
-    page,
-    pageSize,
-  });
+
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    useTodos(pageSize);
 
   if (error) {
     return <h2>{error.message} :(</h2>;
@@ -17,21 +14,22 @@ export default function App() {
     return <h2>Loading...</h2>;
   }
 
+  // console.log(data);
+  const todos = data?.pages.flat();
+
   return (
     <>
       <h2>Todos</h2>
 
       <ul>
-        {data?.map((todo) => (
+        {todos?.map((todo) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
       </ul>
 
-      <button disabled={page == 1} onClick={() => setPage(page - 1)}>
-        {"<<"}
+      <button disabled={isFetchingNextPage} onClick={() => fetchNextPage()}>
+        {isFetchingNextPage ? "Cargando..." : "Cargar más ..."}
       </button>
-      <button onClick={() => setPage(page + 1)}>{">>"}</button>
-      {isPlaceholderData && <span>Cargando...</span>}
     </>
   );
 }
--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,5 +1,9 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import {
+  keepPreviousData,
+  useInfiniteQuery,
+  useQuery,
+} from "@tanstack/react-query";
+import axios, { all } from "axios";
 
 type Todo = {
   id: number;
@@ -8,19 +12,14 @@ type Todo = {
   userId: number;
 };
 
-type TodoQuery = {
-  page: number;
-  pageSize: number;
-};
-
-const queryTodos = (query: TodoQuery): Promise<Todo[]> => {
+const queryTodos = (pageParam: number, pageSize: number): Promise<Todo[]> => {
   const url = "https://jsonplaceholder.typicode.com/todos";
 
   return axios
     .get(url, {
       params: {
-        _start: (query.page - 1) * query.pageSize,
-        _limit: query.pageSize,
+        _start: (pageParam - 1) * pageSize,
+        _limit: pageSize,
       },
     })
     .then((response) => {
@@ -28,14 +27,17 @@ const queryTodos = (query: TodoQuery): Promise<Todo[]> => {
     });
 };
 
-function useTodos(query: TodoQuery) {
+function useTodos(pageSize: number) {
   // la que sigue es una query local y tiene una configuración en particular y no para todas las queries de la App
-  return useQuery({
+  return useInfiniteQuery({
     // usando un formato tipo url para el queryKey, se pueden pasar parámetros a la función queryFn, en este caso el userId, y así se pueden hacer queries dinámicas, por ejemplo para traer los todos de un usuario específico, con esto se puede hacer una query para cada usuario sin necesidad de crear una función para cada uno, solo cambiando el userId en el queryKey
     //users / 2 / todos
-    queryKey: ["todos", query],
-    queryFn: () => queryTodos(query),
-    placeholderData: keepPreviousData, // esto hace que mientras se carga la nueva data, se mantenga la data anterior, en lugar de mostrar un estado de carga vacío, esto es útil para paginación o para filtros, para evitar que se muestre un estado de carga vacío cada vez que se cambia el filtro o la página, y en su lugar mostrar la data anterior hasta que llegue la nueva data, esto mejora la experiencia del usuario al evitar parpadeos o estados de carga vacíos innecesarios;
+    queryKey: ["todos"],
+    queryFn: ({ pageParam }) => queryTodos(pageParam, pageSize),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 }
 
