2f6dd78d2a7ba6fe084baea0600f7f3ed1db663a
uescobar <uescobar.360@gmail.com>
2026-04-15 15:22:13 -0600

    optimistic ui

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,10 @@ export default function App() {
       axios
         .post<Post>("https://jsonplaceholder.typicode.com/posts", post)
         .then((response) => response.data),
-    onSuccess: (savedPost) => {
+    onMutate: (newPost) => {
+      //optimistic update, actualizar la UI antes de que la mutación se complete, para mejorar la experiencia del usuario, pero si la mutación falla, entonces se debe revertir el cambio en la UI, para eso se puede usar el método setQueryData para actualizar los datos de la query en la cache de React Query, y así se actualiza la UI con los datos correctos después de que la mutación falle
       queryClient.setQueryData<Post[]>(["posts"], (post = []) => [
-        savedPost,
+        newPost,
         ...post,
       ]);
 
@@ -31,6 +32,12 @@ export default function App() {
         titleRef.current.value = "";
         bodyRef.current.value = "";
       }
+    },
+    onSuccess: (savedPost, newPost) => {
+      //actualizar la cache de React Query con los datos del post guardado en el servidor, para que la UI se actualice con los datos correctos después de que la mutación se complete, por ejemplo para obtener el id generado por el servidor, entonces si sería necesario invalidar la query para que se vuelva a ejecutar y obtener los datos actualizados del servidor
+      queryClient.setQueryData<Post[]>(["posts"], (post = []) =>
+        post.map((post) => (post.id === newPost.id ? savedPost : post)),
+      );
     },
   });
 
